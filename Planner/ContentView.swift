30851b4a178b4eeabb1942b60c16a7a3c5e92b1b
jocortright <joshua.c.cortright@gmail.com>
2025-10-03 23:17:55 -0700

    Initial Commit

---
+++ Planner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planner
+//
+//  Created by Joshua Cortright on 10/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Joshua Cortright on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
